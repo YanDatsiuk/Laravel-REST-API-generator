179af046eff5f9d9fa8d9aa3f6902850e81dde1c
datsyuk <yan.datsiuk@thinkmobiles.com>
2017-05-23 11:14:42 +0300

    Decoupled method in MakeSwaggerModels command If applied, commit will decouple method in MakeSwaggerModels command

--- src/Commands/MakeSwaggerModelsCommand.php
+++ src/Commands/MakeSwaggerModelsCommand.php
@@ -29,14 +29,14 @@ class MakeSwaggerModelsCommand extends Command
     protected $description = 'Create swagger models.';
 
     /**
-     * List of model names
+     * List of model names.
      *
      * @var array
      */
     private $models = [];
 
     /**
-     * List of table names for gathering schema info
+     * List of table names for gathering schema info.
      *
      * @var array
      */
@@ -56,21 +56,9 @@ class MakeSwaggerModelsCommand extends Command
         //get list of database tables
         $this->tables = explode(',', $this->option('tables'));
 
-        //check whether model names were submitted
-        if (strlen($this->models[0]) === 0) {
-            $this->error('Please specify model names in kebab notation');
+        //validate input data
+        if (!$this->isInputValid($this->models, $this->tables)){
             return;
-        }
-
-        //check whether model names were submitted
-        if (strlen($this->tables[0]) === 0) {
-            $this->error('Please specify table names');
-            return;
-        }
-
-        //check whether table quantity are equal to model names quantity
-        if (count($this->models) !== count($this->tables)) {
-            $this->error('table names quantity are not equal to model names quantity');
         }
 
         //compile swagger models
@@ -82,8 +70,36 @@ class MakeSwaggerModelsCommand extends Command
             ]);
         }
 
-        $this->info('make:swagger-models cmd executed');
+        $this->info('make:swagger-models cmd executed.');
     }
 
-    //todo add function initAndValidate($inputedModels, $inputedTables)
+    /**
+     * Validate input data.
+     *
+     * @param array $models
+     * @param array $tables
+     * @return bool
+     */
+    private function isInputValid(array $models, array $tables)
+    {
+        //check whether model names were submitted
+        if (strlen($models[0]) === 0) {
+            $this->error('Please specify model names in kebab notation.');
+            return false;
+        }
+
+        //check whether model names were submitted
+        if (strlen($tables[0]) === 0) {
+            $this->error('Please specify table names.');
+            return false;
+        }
+
+        //check whether table quantity are equal to model names quantity
+        if (count($models) !== count($tables)) {
+            $this->error('Table names quantity are not equal to model names quantity.');
+            return false;
+        }
+
+        return true;
+    }
 }

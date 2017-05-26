706f0205dddbfdd976e61f547d103e4b40fdf0bf
datsyuk <yan.datsiuk@thinkmobiles.com>
2017-05-26 14:08:21 +0300

    Added seed for action_group table, seed for one super admin and fixed middleware If applied, commit will add seed for action_group table, seed for one super admin and fix middleware

--- src/Database/Seeds/AuthActionGroupsTableSeeder.php
+++ src/Database/Seeds/AuthActionGroupsTableSeeder.php
@@ -2,7 +2,10 @@
 
 namespace TMPHP\RestApiGenerators\Database\Seeds;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class AuthActionGroupsTableSeeder extends Seeder
 {
@@ -13,6 +16,42 @@ class AuthActionGroupsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $modelsNamespace = config('rest-api-generator.namespaces.models');
+        $authActionModel = $modelsNamespace.'\AuthAction';
+        $authGroupModel = $modelsNamespace.'\AuthGroup';
+        $authActionGroupModel = $modelsNamespace.'\AuthActionGroup';
+
+        //get all actions
+        $allActions = $authActionModel::all();
+
+        //get all read actions
+        $readActions = $this->onlyReadActions($allActions);
+
+        //assign all actions to "admin" group
+        $adminGroup = $authGroupModel::firstOrCreate(['name' => 'admin']);
+        $this->assignActionsToGroup($allActions, $adminGroup);
+
+        //assign read actions to "guest" and "registered" group
+        $guestGroup = $authGroupModel::firstOrCreate(['name' => 'guest']);
+        $this->assignActionsToGroup($readActions, $guestGroup);
+
+        $registeredGroup = $authGroupModel::firstOrCreate(['name' => 'registered']);
+        $this->assignActionsToGroup($readActions, $registeredGroup);
+    }
+
+    private function onlyReadActions(Collection $allActions){
+
+        //todo filter
+        return $allActions;
+    }
+
+    private function assignActionsToGroup(Collection $actions, Model $group){
+
+        foreach ($actions as $action){
+            DB::table('auth_action_group')->insert([
+                'action_id' => $action->id,
+                'group_id' => $group->id,
+            ]);
+        }
     }
 }
--- src/Database/Seeds/AuthGroupUsersTableSeeder.php
+++ src/Database/Seeds/AuthGroupUsersTableSeeder.php
@@ -13,6 +13,26 @@ class AuthGroupUsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $modelsNamespace = config('rest-api-generator.namespaces.models');
+        $authAuthGroupUserModel = $modelsNamespace . '\AuthGroupUser';
+        $authGroupModel = $modelsNamespace.'\AuthGroup';
+        $userModel = $modelsNamespace . '\User';
+
+        //create user admin
+        $admin = $userModel::firstOrCreate(
+            [
+                'name' => 'John ADMIN',
+                'email' => 'admin@gmail.com',//todo take from to config
+                'password' => bcrypt('secret'),//todo take from to config
+            ]);
+
+        //create group "admin"
+        $adminGroup = $authGroupModel::firstOrCreate(['name' => 'admin']);
+
+        //assign to user group "admin"
+        $authAuthGroupUserModel::firstOrCreate([
+            'group_id' => $adminGroup->id,
+            'user_id' => $admin->id,
+        ]);
     }
 }
